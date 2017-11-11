d4285e321d74feab7018aae71f47422d0f6ce2a4
Mistra Minayasa <manoyasamistra@gmail.com>
2017-11-11 19:23:26 +0700

    preparing for deploying

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
@@ -18,15 +20,12 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id ])
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             #do something
             flash[:notice] = "Artikel berhasil diedit"
@@ -37,13 +36,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         flash[:notice] = "Artikel berhasil dihapus"
         redirect_to articles_path
     end
 
     private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
     def article_params
         params.require(:article).permit(:title, :description)
     end
