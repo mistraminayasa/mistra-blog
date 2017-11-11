5cf66dba68ea259279fc609b76cd368f9bd46cc0
Mistra Minayasa <manoyasamistra@gmail.com>
2017-11-11 18:05:04 +0700

    menambahakn index, edit, update dan show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
     def new
         @article = Article.new
     end
@@ -17,6 +20,22 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id ])
     end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            #do something
+            flash[:notice] = "Artikel berhasil diedit"
+            redirect_to article_path(@article)
+        else 
+            render 'edit'
+        end
+    end
+
     private
     def article_params
         params.require(:article).permit(:title, :description)
